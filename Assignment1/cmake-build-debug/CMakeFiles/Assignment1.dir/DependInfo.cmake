36905dc2d7f7ceb57036c5e4e84520d453a379f0
Aziz <74883676+azizosharke@users.noreply.github.com>
2022-03-13 17:49:57 +0000

    Add files via upload

---
+++ Assignment1/cmake-build-debug/CMakeFiles/Assignment1.dir/DependInfo.cmake
@@ -0,0 +1,30 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/cygdrive/c/Users/Abdelaziz Abushark/Desktop/College/2ndyr2ndsem/NLP/Assignments/Assignment1/cmake-build-debug/FSA.cpp" "/cygdrive/c/Users/Abdelaziz Abushark/Desktop/College/2ndyr2ndsem/NLP/Assignments/Assignment1/cmake-build-debug/CMakeFiles/Assignment1.dir/FSA.cpp.o"
+  "/cygdrive/c/Users/Abdelaziz Abushark/Desktop/College/2ndyr2ndsem/NLP/Assignments/Assignment1/cmake-build-debug/Node.cpp" "/cygdrive/c/Users/Abdelaziz Abushark/Desktop/College/2ndyr2ndsem/NLP/Assignments/Assignment1/cmake-build-debug/CMakeFiles/Assignment1.dir/Node.cpp.o"
+  "/cygdrive/c/Users/Abdelaziz Abushark/Desktop/College/2ndyr2ndsem/NLP/Assignments/Assignment1/cmake-build-debug/tester.cpp" "/cygdrive/c/Users/Abdelaziz Abushark/Desktop/College/2ndyr2ndsem/NLP/Assignments/Assignment1/cmake-build-debug/CMakeFiles/Assignment1.dir/tester.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ Assignment1/cmake-build-debug/CMakeFiles/Assignment1.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "Assignment1.exe"
+  "Assignment1.pdb"
+  "CMakeFiles/Assignment1.dir/FSA.cpp.o"
+  "CMakeFiles/Assignment1.dir/Node.cpp.o"
+  "CMakeFiles/Assignment1.dir/tester.cpp.o"
+  "libAssignment1.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Assignment1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
